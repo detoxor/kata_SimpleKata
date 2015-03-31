523a5540b6d0ac7bc6a18c8ad339bcc24f71c5a0
Tomas Cejka <dev.cejkatomas@gmail.com>
2015-03-31 20:22:49 +0200

    * jak by mel vypadat starup maven projektu

---
+++ src/main/java/cz/tomascejka/kata/simple/App.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package cz.tomascejka.kata.simple;
+
+/**
+ * 
+ * @author tomas.cejka
+ *
+ */
+public class App 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world...simple");
+	}
+
+}
